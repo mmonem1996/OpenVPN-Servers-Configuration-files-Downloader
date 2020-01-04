cfcc5d98acab38d9298f25c0933ed9c77fe94809
mmonem1996 <hgamal1147>
2020-01-04 08:14:59 +0200

    fixed path issues

--- Get_Servers.py
+++ Get_Servers.py
@@ -5,11 +5,12 @@
 # All rights reserved to Mohammed Abdelmonem, mohammedhll222@gmail.com
 
 import urllib.request, csv, sys, base64
-from os import path
+from os import path, getcwd, chdir
 import platform
 
 VPN_SERVERS_ADDRESS = 'http://www.vpngate.net/api/iphone/'
 SERVERS_COUNT = 10
+CWD = path.dirname(path.realpath( __file__ ))
 
 def to_int(s):
 	try:
@@ -27,14 +28,15 @@ def s_key(row): # sorts the servers
 def update_servers(): # if the user passes '--update' via the commandline, this function would pull the latest servers data 
 	try:
 		servers_csv = urllib.request.urlopen(VPN_SERVERS_ADDRESS).read().decode().split('\n')[1:]
-		with open('./.servers.csv', 'w') as cash_file:
+		with open(path.join(CWD, '.servers.csv'), 'w') as cash_file:
 			print('updating servers...')
 			for row in servers_csv:
 				cash_file.write(row)
 	except Exception as e:
 		print(e)
 
-def load_hosts(hosts_csv, excluded):
+def load_hosts(hosts_csv, excluded = None):
+	if excluded is None: excluded = []
 	with open(hosts_csv, 'r') as cash_file:
 		reader = csv.DictReader(cash_file)
 		rows = [row for row in reader if not row['CountryLong'] in excluded]
@@ -43,26 +45,38 @@ def load_hosts(hosts_csv, excluded):
 
 def create_ovpn_files(servers_count, hosts = None, management_sock_name = None):
 	print('creating ovpn files...')
-	logg = open('./log.txt', 'w')
+	logg = open(path.join(CWD, 'log.txt'), 'w')
 	# due to connection problems arising between most vpn servers located in these two regions and my own ISP, i filtered them out
 	if hosts == None:
-		hosts = load_hosts('./.servers.csv', excluded= ['United States', 'United Kingdom'])
+		hosts = load_hosts(path.join(CWD, '.servers.csv')) # excluded= ['United States', 'United Kingdom']
+
 	for count, row in enumerate(hosts):
 		if count + 1 > servers_count:
 			break
+
+		# print and log basic info of the ovpn file
 		print(f"server {count + 1}: Speed {round(to_int(row['Speed'])/ 1024**2)} Mb/s  sessions {row['NumVpnSessions']}  location  {row['CountryLong']} ping {row['Ping']} ms")
 		logg.write(f"server {count + 1}: Speed {round(to_int(row['Speed'])/ 1024**2)} Mb/s  sessions {row['NumVpnSessions']}  location  {row['CountryLong']} ping {row['Ping']} ms\n")
-		with open(f'./ovpn files/{count + 1}.ovpn', 'w') as ovpn_file:
+
+		# create and write downloaded info to a .ovpn file
+		with open(path.join(CWD, f'ovpn files/{count + 1}.ovpn'), 'w') as ovpn_file:
 			ovpn_file.write("###############################################################################\n")
 			ovpn_file.write(f"# Host Name : {row['#HostName']}\n")
 			ovpn_file.write(f"# IP Address : {row['IP']}\n")
 			ovpn_file.write(f"# Country : {row['CountryLong']}\n")
 			if row['Message'] != '':
 				ovpn_file.write(f"# Message : {row['Message']}\n")
+				logg.write(f"# Message : {row['Message']}\n")
+
+			# Configuration files are Base64 encoded 
 			ovpn_file.write('\n')
 			ovpn_file.write(base64.b64decode(row['OpenVPN_ConfigData_Base64']).decode('utf-8'))
-			if management_sock_name != None:
+
+			# if you want to control OpenVPN via sockets set management_sock_name = (ipaddress, port)
+			if management_sock_name != None: 
 				ovpn_file.write(f'\nmanagement {management_sock_name[0]} {management_sock_name[1]}\n')
+
+			# This bit of code aims to address dns leak issues
 			plt = platform.system()
 			if plt == 'Linux':
 				ovpn_file.write('\nscript-security 2\nup /etc/openvpn/update-resolv-conf\ndown /etc/openvpn/update-resolv-conf')
@@ -76,11 +90,14 @@ def main():
 	global SERVERS_COUNT
 	if '--update' in sys.argv:
 		update_servers()
-	if '--count' in sys.argv: # specifies how many openvpn configuration files to create
+
+	# specifies how many openvpn configuration files to create
+	if '--count' in sys.argv: 
 		index = sys.argv.index('--count') + 1
 		if index < len(sys.argv):
 			num = to_int(sys.argv[index])
 			SERVERS_COUNT = num if num != 0 else SERVERS_COUNT
+
 	create_ovpn_files(SERVERS_COUNT)
 
 if __name__ == '__main__':
